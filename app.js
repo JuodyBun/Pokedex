61ade278edabe72520e8d0223f484dcb5c71e6a5
Juody Bun <bjuody@gmail.com>
2020-03-29 17:06:48 -0500

    Added sprites and text formats

--- app.js
+++ app.js
@@ -9,35 +9,59 @@ const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
 
-console.log(pokeName);
+// Constants and Variables
 
-//API connection; first connect with .json file; then displays Pokemon info
-fetch('https://pokeapi.co/api/v2/pokemon/10')
+const TYPES = [
+    'normal', 'fighting', 'flying',
+    'poison', 'ground', 'rock',
+    'bug', 'ghost', 'steel',
+    'fire', 'water', 'grass',
+    'electric', 'psychic', 'ice',
+    'dragon', 'dark', 'fairy'
+];
+
+// Functions
+
+// Will capitalize first letter of string
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
+// Will remove last instance of Pokemon
+const resetScreen = () => {
+    mainScreen.classList.remove('hide');
+    for (const type of TYPES) {
+        mainScreen.classList.remove(type);
+    }
+};
+
+// API connection; first connect with .json file; then displays Pokemon info
+fetch('https://pokeapi.co/api/v2/pokemon/1 ')
     .then(res =>  res.json())
     .then(data => {
-        console.log(data);
-        mainScreen.classList.remove('hide');
-        //Display's name, id, height, and weight
-        pokeName.textContent = data['name'];
-        pokeId.textContent = data['id'];
-        pokeWeight.textContent = data['weight'];
-        pokeHeight.textContent = data['height'];
+        resetScreen();
 
         //Displays Pokemon's data type(s)
         const dataTypes = data['types'];
         const dataFirstType = dataTypes[0];
         const dataSecondType = dataTypes[1];
-        pokeTypeOne.textContent = dataTypes[0]['type']['name'];
+        pokeTypeOne.textContent = capitalize(dataTypes[0]['type']['name']);
         //If second data type exists, it will display, if not it will hide
         if (dataSecondType) {
             pokeTypeTwo.classList.remove('hide');
-            pokeTypeTwo.textContent = dataSecondType['type']['name'];
+            pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
         } else  {
             pokeTypeTwo.classList.add('hide');
             pokeTypeTwo.textContent = '';
         }
+        //Changes the background color to Pokemon's type
+        mainScreen.classList.add(dataFirstType['type']['name']);
 
-        //Displays Pokemon's sprite(s) if exists
+        //Display's name, id, height, and weight
+        pokeName.textContent = capitalize(data['name']);
+        pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+        pokeWeight.textContent = data['weight'];
+        pokeHeight.textContent = data['height'];
+
+        //Displays Pokemon's sprite(s) if exists 
         pokeFrontImage.src = data['sprites']['front_default'] || '';
         pokeBackImage.src = data['sprites']['back_default'] || '';
     });
