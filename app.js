d4568ac8ec8303abcb172cca1a83733f4febff5a
Juody Bun <bjuody@gmail.com>
2020-05-26 21:57:24 -0500

    Declared values for second main screen

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
+
 const pokeListItems = document.querySelectorAll('.list-item');
 const leftButton = document.querySelector('.left-button');
 const rightButton = document.querySelector('.right-button');
 
+const mainScreen2 = document.querySelector('.main-screen2'); // Second main screen
 const aButton = document.querySelector('.a-button'); // Select for further info
 const bButton = document.querySelector('.b-button'); // Returns back to Pokemon's main screen
+const ability1 = document.querySelector('.ability-1'); // First ability
+const ability2 = document.querySelector('.ability-2'); // Second ability
 
 // Constants and Variables
 const TYPES = [
@@ -26,16 +30,26 @@ const TYPES = [
 ];
 let prevUrl = null;
 let nextUrl = null;
+let moreUrl = null;
+let returnUrl = null;
 
 // FUNCTIONS
 // Will capitalize first letter of string
 const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
 
-// Will remove last instance of Pokemon
+// Will remove last instance of Pokemon on left main screen
 const resetScreen = () => {
     mainScreen.classList.remove('hide');
     for (const type of TYPES) {
         mainScreen.classList.remove(type);
+    }
+};
+
+// Will remove last instance of Pokemon more detail on left main screen
+const resetScreen2 = () => {
+    mainScreen2.classList.remove('hide');
+    for (const type of TYPES) {
+        mainScreen2.classList.remove(type);
     }
 };
 
@@ -64,10 +78,52 @@ const fetchPokeList = url => {
             }
         }
     });
-}
+};
 
 // Get data for left-side of script - each Pokemon
 const fetchPokeData = id => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(res =>  res.json())
+    .then(data => {
+        resetScreen();
+
+        //Declare values of Pokemon's data type(s)
+        const dataTypes = data['types'];
+        const dataFirstType = dataTypes[0];
+        
+        //If second data type exists, it will display, if not it will hide
+        if(dataTypes[1]) {
+            const dataFirstType = dataTypes[1];
+            const dataSecondType = dataTypes[0];
+            pokeTypeTwo.classList.remove('hide');
+            pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
+            pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
+            mainScreen.classList.add(dataFirstType['type']['name'])
+        }
+        else {
+            const dataFirstType = dataTypes[0];
+            pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
+            pokeTypeTwo.classList.add('hide');
+            pokeTypeTwo.textContent = "";
+            mainScreen.classList.add(dataFirstType['type']['name'])
+        }
+        //Changes the background color to Pokemon's type
+        mainScreen.classList.add(dataFirstType['type']['name']);
+
+        //Display's name, id, height, and weight
+        pokeName.textContent = capitalize(data['name']);
+        pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+        pokeWeight.textContent = data['weight'];
+        pokeHeight.textContent = data['height'];
+
+        //Displays Pokemon's sprite(s) if exists 
+        pokeFrontImage.src = data['sprites']['front_default'] || '';
+        pokeBackImage.src = data['sprites']['back_default'] || '';
+    });
+}
+
+// Get data for left-side of script - each Pokemon MORE DETAILS!!
+const fetchPokeData2 = id => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then(res =>  res.json())
     .then(data => {
@@ -134,8 +190,18 @@ const handleListItemClick = (e) => {
 };
 
 // A-button click for further detail of Pokemon (left-screen)
+const handleAButtonClick = () => {
+    if (moreUrl) {
+        fetchPokeList(moreUrl);
+    }
+};
 
 // B-button click to return to main Pokemon screen (left-screen)
+const handleBButtonClick = () => {
+    if (returnUrl) {
+        fetchPokeList(returnUrl);
+    }
+};
 
 // Displays further detail of Pokemon 
 
@@ -144,7 +210,14 @@ leftButton.addEventListener('click', handleLeftButtonClick);
 rightButton.addEventListener('click', handleRightButtonClick);
 for (const pokeListItem of pokeListItems) {
     pokeListItem.addEventListener('click', handleListItemClick)
-}
+};
+
+aButton.addEventListener('click', handleAButtonClick);
+bButton.addEventListener('click', handleBButtonClick);
+//CHANGE BOTTOM TO NEW SCREEN
+for (const pokeListItem of pokeListItems) {
+    pokeListItem.addEventListener('click', handleListItemClick)
+};
 
 // Initialize App
 fetchPokeList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
